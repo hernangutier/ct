35fb4110360ca10249c0798d0c12ef943f4db76c
hernangutier <hernangutier@gmail.com>
2024-11-19 04:22:54 -0600

    Agregado de Comentario e Identacion

--- ct_list_price/models/ContainerCateg.py
+++ ct_list_price/models/ContainerCateg.py
@@ -1,9 +1,15 @@
 from odoo import fields, models, api
 
-
+#--------------------------------------------------------------------
+#
+# Contenedor de Categorias para Listas de Precios Configuradas
+#
+#--------------------------------------------------------------------
 class ContainerCategory(models.Model):
     _name = 'ct.list.price.container.category'
     _description = 'Contenedor de Categorias'
+    #--- Nombre de la Lista
     name = fields.Char('Denominacion', required=True)
+    #---- Categorias Hijas a Mostrar
     categ_childs_ids = fields.Many2many(
         'product.category', string='Categorias Hijas')
